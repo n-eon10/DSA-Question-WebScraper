7a57a6a2deef1ef728cc85a70af09e16493be994
Afam <78809835+neon-gpt@users.noreply.github.com>
2023-10-29 09:42:12 +0000

    created get question info from title function

--- main/main.py
+++ main/main.py
@@ -1,40 +1,67 @@
 import requests
-from bs4 import BeautifulSoup
+import json
+from bs4 import BeautifulSoup, NavigableString
+import html2text
 
+def html_to_markdown(html):
 
-url = "https://leetcode.com/graphql"
-
-headers = {
-  'Content-Type': 'application/json',
-}
-
-data = {
-  'operationName': 'questionInfo',
-  'query':
-  '''
-  query questionInfo($titleSlug: String!) {
-    question(titleSlug: $titleSlug) {
-      questionFrontendId
-      title
-      difficulty
-      content
-      likes
-      dislikes
-      stats
-      isPaidOnly
-      }
-  }
-  ''',
-  'variables': {'titleSlug': "two-sum"}
-}
-
-response = requests.get(url, json=data, headers=headers)
-
-if response.status_code == 200:
+    h = html2text.HTML2Text()
+    h.ignore_links = True
     
-  soup = BeautifulSoup(response.text, 'html.parser')
-  print(soup.prettify())
+    return h.handle(html)
 
-else:
+def get_question_info_from_title(question_title_slug: str):
+    
+    url = 'https://leetcode.com/graphql'
 
-  print("Failed to retrieve the web page. Status code:", response.status_code)
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    data = {
+        'operationName': 'questionInfo',
+        'query':
+        '''
+        query questionInfo($titleSlug: String!) {
+            question(titleSlug: $titleSlug) {
+                questionFrontendId
+                title
+                difficulty
+                content
+                likes
+                dislikes
+                stats
+                isPaidOnly
+            }
+        }
+        ''',
+        'variables': {'titleSlug': question_title_slug}
+
+    }
+
+    response = requests.post(url, json=data, headers=headers, timeout=10)
+
+    response_data = response.json()
+    difficulty = response_data['data']['question']['difficulty']
+    title = response_data['data']['question']['title']
+    question_content = response_data['data']['question']['content']
+
+
+    example_position = question_content.find(
+        '<strong class="example">')
+    constraint_query_string = '<p><strong>Constraints:</strong></p>'
+    constraints_position = question_content.find(constraint_query_string)
+
+    description = question_content[:example_position]
+
+    constraints = question_content[constraints_position + len(constraint_query_string):]
+
+    description = html_to_markdown(description)
+    constraints = html_to_markdown(constraints)
+
+    print(difficulty, title, description, constraints)
+
+    return difficulty, title, description, constraints
+
+
+get_all_question_slugs()
